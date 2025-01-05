57f9dad3903df6e335aded4013fda9fc0747188d
Raka Rendra Fayanto <l200224246@student.ums.ac.id>
2025-01-06 05:50:54 +0700

    fix web

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ums-l200224246.github.io',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/ums-l200224246.github.io',
+  
 });
